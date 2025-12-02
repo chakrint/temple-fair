3456a8ad118fe274a5def87e8273c16a831f5aff
ZL2566 <123916326+chakrint@users.noreply.github.com>
2025-12-02 16:37:44 +0700

    Enhance tailwind.config.js with comments
    Added comments to clarify the purpose of content paths.

--- dependencies/tailwind.config.js
+++ dependencies/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",       // สแกนไฟล์ใน folder app
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // สแกนไฟล์ใน folder components
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
